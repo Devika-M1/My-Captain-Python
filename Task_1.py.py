65359c80c6e3be13e37b1ae8d9111f65e74c6195
Devika M <97176177+Devika-M1@users.noreply.github.com>
2022-01-17 16:14:17 +0530

    Add files via upload

---
+++ Task_1.py.py
@@ -0,0 +1,3 @@
+filename = input("Input the filename: ");
+extns = filename.split(".")
+print("The extension of file is:" ,(extns[-1]))
---
+++ Task_2.py.py
@@ -0,0 +1,4 @@
+filename = input("Input the filename: ");
+extns = filename.split(".")
+print("The extension of file is:" ,(extns[-1]))
+
