a4f0bf28fe8c7ece8990a2762148f447dd9bf1cb
Devika M <97176177+Devika-M1@users.noreply.github.com>
2022-01-26 12:39:48 +0530

    Add files via upload

---
+++ Loop_2.py
@@ -0,0 +1,16 @@
+# Python program to print positive numbers from list
+
+# list of numbers
+list1 = [12, -7, 5, 64, -14]
+
+# iterating each number in list
+pos_nos = [num for num in list1 if num >= 0]
+print(pos_nos)  
+# Python program to print positive numbers from list
+
+# list of numbers
+list2 = [12, 14, -95,3]
+
+# iterating each number in list
+pos_nos = [num for num in list2 if num >= 0]
+print(pos_nos)
